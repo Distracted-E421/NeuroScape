bf4d0ecfde402f1458109aabb2cc0b8e28a5d23c
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-10-15 15:18:06 -0500

    adding update and delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -22,11 +22,23 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/update/<int:task_id>', methods=['POST'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.title = request.form['title']
+    task.complete = request.form.get('complete', False)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 if __name__ == "__main__":
     db.create_all()
-    
-    new_task1 = Task(title="New Task #1", complete=False)
-    db.session.add(new_task1)
-    db.session.commit()
-    
     app.run(debug=True)
