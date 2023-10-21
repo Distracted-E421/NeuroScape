6057ab5845911f67eab8c579a355fc30b12085c0
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-10-21 12:35:25 -0500

    1.finished for now...
    Ill be back to it on Monday

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-
+# Importing required libraries and modules
 from flask import Flask, render_template, request, redirect, url_for
+from oauthlib.oauth2 import WebApplicationClient
 from flask_migrate import Migrate
 from db import db, Task, init_db
-from google_api import init_google_client, get_user_token, add_event
+from google_api import get_user_token, add_event
 import requests
 from fastapi.security import OAuth2AuthorizationCodeBearer
-from oauthlib.oauth2 import WebApplicationClient
 import json
 
-
+# Initialize Flask app and database migration
 app = Flask(__name__,)
 migrate = Migrate(app, db)
 
 # Initialize Google OAuth with FastAPI OAuth2
-
-oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl='https://oauth2.googleapis.com/token', authorizationUrl='https://accounts.google.com/o/oauth2/auth')
-authorizationUrl='https://accounts.google.com/o/oauth2/auth',
-tokenUrl='https://accounts.google.com/o/oauth2/token',
-clientId='80048219165-jhs4kk14n3iub1mn56vgkicc4si9fvph.apps.googleusercontent.com',
-clientSecret='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',
-scopes=['openid', 'email', 'profile']
 
 with open('client_secret.json', 'r') as f:
     GOOGLE_CLIENT_INFO = json.load(f)
@@ -34,24 +27,18 @@ client = WebApplicationClient(CLIENT_ID)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize Google OAuth settings
+oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl='https://oauth2.googleapis.com/token', authorizationUrl='https://accounts.google.com/o/oauth2/auth')
+authorizationUrl='https://accounts.google.com/o/oauth2/auth',
+tokenUrl='https://accounts.google.com/o/oauth2/token',
+clientId='80048219165-jhs4kk14n3iub1mn56vgkicc4si9fvph.apps.googleusercontent.com',
+clientSecret='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',
+scopes=['openid', 'email', 'profile']
 # Initialize db
 init_db(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/google-login')
-def google_login():
-    # Generate OAuth2 authorization URI
-    request_uri = client.prepare_request_uri(
-        GOOGLE_CLIENT_INFO['web']['auth_uri'],
-        redirect_uri=REDIRECT_URI,
-        scope=['openid', 'email', 'profile']
-    )
-    return redirect(request_uri)
-
-@app.route('/callback')
+# TODO: Add logic for Google OAuth callback
 def callback():
     # Get authorization code
     code = request.args.get('code')
@@ -71,9 +58,13 @@ def callback():
     user_info = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', headers={'Authorization': f'Bearer {access_token}'}).json()
 
     # TODO: Store user info in database
-    # You can now store the user_info in your database
 
     return 'Logged in as: ' + user_info['email'], 200
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- google_api.py
+++ google_api.py
@@ -1,36 +1,24 @@
+
+
+# Importing required libraries and modules
 from google.oauth2.credentials import Credentials
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from google.auth.transport.requests import Request
 from flask import current_app, redirect, url_for, session
-from app import db, User
 import json
 import os
 
-
-# Define the scopes
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+# Define the scopes and initialize the Google API client
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 # Initialize the Google API client
-flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secret.json', SCOPES)
-
-def init_google_client():
-    client_id = current_app.config.get('GOOGLE_CLIENT_ID')
-    client_secret = current_app.config.get('GOOGLE_CLIENT_SECRET')
-    if not os.path.exists('token.json'):
-        flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(json.dumps(flow.credentials.to_json()))
-    else:
-        with open('token.json', 'r') as token:
-            creds_data = json.loads(token.read())
-        flow.credentials = google.oauth2.credentials.Credentials.from_authorized_user_info(creds_data)
-
-    return flow
 
 
+# TODO: Implement OAuth2 flow to get user token and store it in the database
+# This function will be called during account creation and login.
+# It will redirect the user to a Google sign-in page (HTML template: google_signin.html)
+# and then handle the OAuth2 callback to get and store the user token.
 def get_user_token():
         # TODO: Implement OAuth2 flow to get user token and store it in the database
         # This function will be called during account creation and login.
